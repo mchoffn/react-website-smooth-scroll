d2b8607af9e2a12e1f9eff0735275c12d81dec66
Mchoffn <mchoffn@hotmail.com>
2021-07-19 13:23:00 +0200

    update hero section

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,13 +1,42 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Video from '../../video/video.mp4'
-import { HeroContainer, HeroBg, VideoBg } from './HeroElements'
+import {Button} from '../ButtonElement'
+import { 
+    HeroContainer, 
+    HeroBg, 
+    VideoBg, 
+    HeroContent, 
+    HeroBtnWrapper, 
+    HeroP, 
+    HeroH1,
+    ArrowForward,
+    ArrowRight
+ } from './HeroElements'
 
 const HeroSection = () => {
+
+    const[hover, setHover] = useState(false)
+
+    const onHover = () => {
+        setHover(!hover)
+    }
+
     return (
         <HeroContainer>
             <HeroBg>
                 <VideoBg autoPlay loop muted src={Video} type='video/mp4'/>
             </HeroBg>
+            <HeroContent>
+                <HeroH1>Virtual Banking Made Easy</HeroH1>
+                <HeroP>
+                    Sign up here for a new account today and recieve 250$ in credit towards payment.
+                </HeroP>
+                <HeroBtnWrapper>
+                    <Button to="signup" onMouseEnter={onHover} onMouseLeave={onHover}>
+                        Get started {hover ? <ArrowForward /> : <ArrowRight />}
+                    </Button>
+                </HeroBtnWrapper>
+            </HeroContent>
         </HeroContainer>
     )
 }
