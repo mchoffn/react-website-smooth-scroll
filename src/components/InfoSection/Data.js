2119366364b53df729da49d239082185a6acb30f
Mchoffn <mchoffn@hotmail.com>
2021-08-04 00:52:00 +0200

    opdate af sections

--- src/components/InfoSection/Data.js
+++ src/components/InfoSection/Data.js
@@ -8,7 +8,41 @@ export const homeObjOne = {
     description:'Get acces to our exclusive app that allows you to send unlimited transacations without getting charged any fees.',
     buttonLabel:'Get started',
     imgStart:false,
+    img: require('../../images/svg-2.svg').default,
+    alt:'Car',
+    dark:true,
+    primary:true,
+    darkText:false
+};
+
+export const homeObjTwo = {
+    id:'about',
+    lightBg:false,
+    lightText:true,
+    lightTextDesc:true,
+    topLine:'Premium Bank',
+    headline:'Unlimited Transactions with zero fees',
+    description:'Get acces to our exclusive app that allows you to send unlimited transacations without getting charged any fees.',
+    buttonLabel:'Get started',
+    imgStart:false,
     img: require('../../images/svg-1.svg').default,
+    alt:'Car',
+    dark:true,
+    primary:true,
+    darkText:false
+};
+
+export const homeObjThree = {
+    id:'about',
+    lightBg:false,
+    lightText:true,
+    lightTextDesc:true,
+    topLine:'Premium Bank',
+    headline:'Unlimited Transactions with zero fees',
+    description:'Get acces to our exclusive app that allows you to send unlimited transacations without getting charged any fees.',
+    buttonLabel:'Get started',
+    imgStart:false,
+    img: require('../../images/svg-3.svg').default,
     alt:'Car',
     dark:true,
     primary:true,
