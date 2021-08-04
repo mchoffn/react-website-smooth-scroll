adb2a56359d64780433f9fddba022ec07aeeae1f
Mchoffn <mchoffn@hotmail.com>
2021-08-05 00:37:04 +0200

    footer, services og social media

---
+++ src/components/Footer/index.js
@@ -0,0 +1,80 @@
+import React from 'react'
+import { FaFacebook, FaInstagram, FaTwitch, FaYoutube } from 'react-icons/fa'
+import {
+    FooterContainer,
+    FooterWrap,
+    FooterLink,
+    FooterLinkItems,
+    FooterLinkTitle,
+    FooterLinksContainer,
+    FooterLinksWrapper
+} from './FooterElements'
+
+const Footer = () => {
+    return (
+        <FooterContainer>
+            <FooterWrap>
+                <FooterLinksContainer>
+                    <FooterLinksWrapper>
+                        <FooterLinkItems>
+                            <FooterLinkTitle>About us</FooterLinkTitle>
+                                <FooterLink to="/signin">How it works</FooterLink>
+                                <FooterLink to="/signin">Testemonials</FooterLink>
+                                <FooterLink to="/signin">Careers</FooterLink>
+                                <FooterLink to="/signin">Investors</FooterLink>
+                                <FooterLink to="/signin">Terms of Service</FooterLink>
+                        </FooterLinkItems>
+                        <FooterLinkItems>
+                            <FooterLinkTitle>Contact Us</FooterLinkTitle>
+                                <FooterLink to="/signin">Contact</FooterLink>
+                                <FooterLink to="/signin">Testemonials</FooterLink>
+                                <FooterLink to="/signin">Careers</FooterLink>
+                                <FooterLink to="/signin">Investors</FooterLink>
+                                <FooterLink to="/signin">Terms of Service</FooterLink>
+                        </FooterLinkItems>
+                    </FooterLinksWrapper>
+                    <FooterLinksWrapper>
+                        <FooterLinkItems>
+                            <FooterLinkTitle>Videos</FooterLinkTitle>
+                                <FooterLink to="/signin">How it works</FooterLink>
+                                <FooterLink to="/signin">Testemonials</FooterLink>
+                                <FooterLink to="/signin">Careers</FooterLink>
+                                <FooterLink to="/signin">Investors</FooterLink>
+                                <FooterLink to="/signin">Terms of Service</FooterLink>
+                        </FooterLinkItems>
+                        <FooterLinkItems>
+                            <FooterLinkTitle>Social Media</FooterLinkTitle>
+                                <FooterLink to="/signin">Facebook</FooterLink>
+                                <FooterLink to="/signin">Instagram</FooterLink>
+                                <FooterLink to="/signin">Twitter</FooterLink>
+                                <FooterLink to="/signin">YouTube</FooterLink>
+                        </FooterLinkItems>
+                    </FooterLinksWrapper>
+                </FooterLinksContainer>
+                <SocialMedia>
+                    <SocialMediaWrap>
+                        <SocialLogo to="/">dolla</SocialLogo>
+                        <WebsiteRights>dolla &copy; {new Date().getFullYear()}
+                        All rights reserved.</WebsiteRights>
+                        <SocialIcons>
+                            <SocialIconLink href="/" target="_blank" aria-label="Facebook">
+                                <FaFacebook />
+                            </SocialIconLink>
+                            <SocialIconLink href="/" target="_blank" aria-label="Instagram">
+                                <FaInstagram />
+                            </SocialIconLink>
+                            <SocialIconLink href="/" target="_blank" aria-label="Twitter">
+                                <FaTwitter />
+                            </SocialIconLink>
+                            <SocialIconLink href="/" target="_blank" aria-label="YouTube">
+                                <FaYoutube />
+                            </SocialIconLink>
+                        </SocialIcons>
+                    </SocialMediaWrap>
+                </SocialMedia>
+            </FooterWrap>
+        </FooterContainer>
+    )
+}
+
+export default Footer
--- src/components/InfoSection/Data.js
+++ src/components/InfoSection/Data.js
@@ -16,32 +16,32 @@ export const homeObjOne = {
 };
 
 export const homeObjTwo = {
-    id:'about',
-    lightBg:false,
-    lightText:true,
-    lightTextDesc:true,
-    topLine:'Premium Bank',
-    headline:'Unlimited Transactions with zero fees',
+    id:'discover',
+    lightBg:true,
+    lightText:false,
+    lightTextDesc:false,
+    topLine:'Unlimited Access',
+    headline:'Get access from anywhere in the world',
     description:'Get acces to our exclusive app that allows you to send unlimited transacations without getting charged any fees.',
-    buttonLabel:'Get started',
+    buttonLabel:'Learn More',
     imgStart:false,
     img: require('../../images/svg-1.svg').default,
     alt:'Car',
-    dark:true,
-    primary:true,
-    darkText:false
+    dark:false,
+    primary:false,
+    darkText:true
 };
 
 export const homeObjThree = {
-    id:'about',
+    id:'signup',
     lightBg:false,
     lightText:true,
-    lightTextDesc:true,
-    topLine:'Premium Bank',
-    headline:'Unlimited Transactions with zero fees',
+    lightTextDesc:false,
+    topLine:'Unlimited support',
+    headline:'We got you covered from anywhere',
     description:'Get acces to our exclusive app that allows you to send unlimited transacations without getting charged any fees.',
-    buttonLabel:'Get started',
-    imgStart:false,
+    buttonLabel:'Start Now',
+    imgStart:true,
     img: require('../../images/svg-3.svg').default,
     alt:'Car',
     dark:true,
